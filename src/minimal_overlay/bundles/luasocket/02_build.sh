c1f0a16c52fdc584771f4872a97a3160de5fe705
bauen1 <j2468h@gmail.com>
2017-07-06 22:41:56 +0200

    Fixed luasocket not building

--- src/minimal_overlay/bundles/luasocket/02_build.sh
+++ src/minimal_overlay/bundles/luasocket/02_build.sh
@@ -10,11 +10,13 @@ cd $WORK_DIR/overlay/luasocket
 
 DESTDIR="$PWD/luasocket_installed"
 
-# Change to the Lua source directory which ls finds, e.g. 'lua-5.3.4'.
-cd $(ls -d lua-*)
+# Change to the luasocket source directory which ls finds, e.g. 'luasocket-5.3.4'.
+cd $(ls -d luasocket-*)
 
 LUA_DIR="$WORK_DIR/overlay/lua/lua_installed"
 CC="$CC -I$LUA_DIR/usr/include -L$LUA_DIR/usr/lib"
+
+# luaL_checkint has been removed in lua 5.3
 CFLAGS="$CFLAGS -fPIC -DLUASOCKET_API='__attribute__((visibility(\"default\")))' -D 'luaL_checkint(L, n)=((int)luaL_checkinteger(L, n))'"
 
 echo "Preparing luasocket work area. This may take a while..."
